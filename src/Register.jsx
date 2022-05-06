6edb30aaa66301e7fb84977ede7ee5cd9ee16339
ThalissonMelo <tallisonmelo@hotmail.com>
2022-05-05 21:40:19 -0300

    Fix register layout page

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,38 +1,29 @@
 import * as React from "react";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 
 function Register(){
   return(
-      <Container maxWidth="sm" sx={{ p: 2, border: '1px smoothed black' }}>
-        <h1>Register</h1>
-        Name:
-        <br/>
-        <TextField id="outlined-basic" label="Type your name" variant="outlined" />
-        <br/>
-        e-mail:
-        <br/>
-        <TextField id="outlined-basic" label="Type your e-mail" variant="outlined" />
-        <br/>
-        Phone:
-        <br/>
-        <TextField id="outlined-basic" label="Type your phone" variant="outlined" />
-        <br/>
-        CPF:
-        <br/>
-        <TextField id="outlined-basic" label="Type your CPF" variant="outlined" />
-        <br/>
-        <br/>
-        <Button variant="contained" color="secondary">Done</Button>
-        
-      </Container>
-
+    <Container maxWidth="sm">
+      <Stack spacing={2} justifyContent="center">
+        <h1>Criar Conta</h1>
+        <p>Nome:</p>
+        <TextField size="small" id="outlined-basic" label="Type your name" variant="outlined" />
+        <p>E-mail:</p>
+        <TextField size="small" id="outlined-basic" label="Type your e-mail" variant="outlined" />
+        <p>Telefone:</p>
+        <TextField size="small" id="outlined-basic" label="Type your phone" variant="outlined" />
+        <p>CPF:</p>
+        <TextField size="small" id="outlined-basic" label="Type your CPF" variant="outlined" />
+        <p>Senha:</p>
+        <TextField size="small" id="outlined-basic" label="Type your CPF" variant="outlined" />
+        <Button variant="contained" color="secondary">Registrar</Button>
+      </Stack>
+    </Container>
   )
 }
 export default Register;
-
-
-
